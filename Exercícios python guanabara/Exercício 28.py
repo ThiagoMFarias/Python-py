42fa23c0887456a64252458a1a867351086dd720
Thiago <FARIAS_CE@YAHOO.COM.BR>
2023-04-17 20:52:29 -0300

    Add

--- Exercícios python guanabara/Exercício 28.py
+++ Exercícios python guanabara/Exercício 28.py
@@ -1,8 +1,14 @@
-import random
+from random import randint
+import time
 
+
+print('-=' * 30)
 print('O computador está escolhendo um número entre 0 e 5...')
-num = random.randrange(0, 5)
+print('-=' * 30)
+num = randint(0, 5)
 descubra = input("Digite qual número você acha que a máquina escolheu: ")
+print('PROCESSANDO...')
+time.sleep(2)
 
 if descubra == num:
     print('Parabéns, o número escolhido pela máquina foi {}'.format(num))
--- Exercícios python guanabara/Exercício 29.py
+++ Exercícios python guanabara/Exercício 29.py
@@ -1,3 +1,5 @@
+# Escreva um programa que leia a velocidade de um carro. se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por cada km acima do limite. 
+
 speed = int(input('Qual a velocidade do carro?'))
 multa = (speed - 80) * 7
 if speed > 80:
