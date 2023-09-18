ffc7ff891be4964ea4f278946c1bc8e330c876cb
Thiago <FARIAS_CE@YAHOO.COM.BR>
2023-09-18 15:35:24 -0300

    Add

--- Exercícios python guanabara/Exercício 56.py
+++ Exercícios python guanabara/Exercício 56.py
@@ -5,13 +5,27 @@
 from datetime import date
 
 somaidade = 0
-mediaidade = 0
+media_idade = 0
+maior_idade_homem= 0
+nome_velho = ''
+total_mulher20 = 0
+
 for pessoa in range(1,5):
     print('----- {}ª PESSOA -----'.format(pessoa))
-    nome = input('Qual o seu nome? ')
-    sexo = input('Qual o seu sexo? [M] para mulher e [H] para homem. ').upper()
+    nome = input('Qual o seu nome? ').strip()
+    sexo = input('Qual o seu sexo? [M] para mulher e [H] para homem. ').upper().strip()
     idade = int(input('Qual a sua idade? '))
     somaidade += idade
+    if pessoa == 1 and sexo in 'Hh':
+        maior_idade_homem = idade
+        nome_velho = nome
+    if sexo in 'Hh' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        nome_velho = nome
+    if sexo in 'Mm' and idade < 21:
+        total_mulher20 += 1
 
-mediaidade = somaidade/4
-print('A média da idade do grupo é {}'.format(mediaidade))
+media_idade = somaidade/4
+print('A média da idade do grupo é {}'.format(media_idade))
+print('O homem mais velho é o {} e sua idade é de {} anos.'.format(nome_velho, maior_idade_homem))
+print('O total de mulheres menores que 20 anos são {}.'.format(total_mulher20))
