7e0b890a57b31193ea3c868ab4eb51275a62e98e
Thiago <FARIAS_CE@YAHOO.COM.BR>
2024-09-19 13:38:08 -0300

    Commit

--- Exercícios python guanabara/Exercício 91.py
+++ Exercícios python guanabara/Exercício 91.py
@@ -1,24 +1,27 @@
 '''Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário. NO final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.'''
 
 from random import randint
+from time import sleep
+from operator import itemgetter
 
-jogadores = dict()
-jogadas = list()
-maior = menor = 0
 print('-='*20)
 print(f'{"JOGO DO DADINHO":^39}')
 print('-='*20)
 
-for c in range(0,4):
-    jogadores['NOME'] = str(input('Qual o nome do primeiro jogador? ').title())
-    num = randint(1,6)
-    if c == 0:
-        maior = num
-    else:
-        if num >= maior:
-            maior = num
-    jogadores['JOGADA'] = num
-    jogadas.append(jogadores.copy())
+jogo = {str(input('Primeiro jogador:').title()): randint(1,6),
+        str(input('Segundo jogador: ').title()): randint(1,6),
+        str(input('Terceiro jogador: ').title()): randint(1,6),
+        str(input('Quarto jogador: ').title()): randint(1,6)}
+
+ranking = list()
+print('Valores sorteados:')
+
+for k, v in jogo.items():
+    print(f'{k} tirou o valor {v} no dado.')
+    sleep(1)
+ranking = sorted(jogo.items(), key = itemgetter(1), reverse=True)
+print(ranking)
+for i, v in enumerate(ranking):
+    print(f'{i + 1}º lugar: {v[0]} com {v[1]} pontos.')
+    sleep(1)
     
-print(f'{dict(sorted(jogadores.items(),))}')
-print(maior)
